d97a3d2913ac8e1d875a9336f892472a21f16342
Phil Ellison <phil.j.ellison@gmail.com>
2019-09-01 20:44:38 +0100

    Add rotation

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ imagecli
 
 A command line image processing tool, built on top of [image](https://github.com/image-rs/image) and [imageproc](https://github.com/image-rs/imageproc).
 
+Very WIP.
+
 <pre>
 cargo run --release -- -v -i images/robin.jpg -o images/morphed.png -p 'gray > gaussian 10.0 > scale 0.3'
 </pre>
--- src/stage.rs
+++ src/stage.rs
@@ -1,6 +1,9 @@
 
-use image::{DynamicImage, GenericImageView, imageops::FilterType};
-use imageproc::filter::gaussian_blur_f32;
+use image::{DynamicImage, GenericImageView, imageops::FilterType, Luma, LumaA, Rgb, Rgba, Bgr, Bgra};
+use imageproc::{
+    filter::gaussian_blur_f32,
+    geometric_transformations::{Interpolation, rotate_about_center},
+};
 
 #[derive(Debug)]
 pub enum Stage {
@@ -9,7 +12,9 @@ pub enum Stage {
     /// Apply a Gaussian blur with the given standard deviation.
     Gaussian(f32),
     /// Convert to grayscale.
-    Gray
+    Gray,
+    /// Rotate clockwise about the image's center by the given angle in degrees.
+    Rotate(f32)
 }
 
 impl Stage {
@@ -24,6 +29,9 @@ impl Stage {
             },
             Self::Gray => {
                 image.grayscale()
+            },
+            Self::Rotate(t) => {
+                image.rotate(*t)
             }
         }
     }
@@ -34,6 +42,7 @@ impl Stage {
             "scale" => Stage::Scale(split[1].parse().unwrap()),
             "gaussian" => Stage::Gaussian(split[1].parse().unwrap()),
             "gray" => Stage::Gray,
+            "rotate" => Stage::Rotate(split[1].parse().unwrap()),
             _ => panic!("Unrecognised stage name {}", split[0]),
         }
     }
@@ -41,6 +50,7 @@ impl Stage {
 
 trait ImageExt {
     fn gaussian(&self, sigma: f32) -> Self;
+    fn rotate(&self, theta: f32) -> Self;
 }
 
 impl ImageExt for DynamicImage {
@@ -53,6 +63,31 @@ impl ImageExt for DynamicImage {
             ImageRgba8(image) => ImageRgba8(gaussian_blur_f32(image, sigma)),
             ImageBgr8(image) => ImageBgr8(gaussian_blur_f32(image, sigma)),
             ImageBgra8(image) => ImageBgra8(gaussian_blur_f32(image, sigma)),
+        }
+    }
+
+    fn rotate(&self, theta: f32) -> Self {
+        use DynamicImage::*;
+        let rad = theta * std::f32::consts::PI / 180.0;
+        match self {
+            ImageLuma8(image) => ImageLuma8(
+                rotate_about_center(image, rad, Interpolation::Bilinear, Luma([0]))
+            ),
+            ImageLumaA8(image) => ImageLumaA8(
+                rotate_about_center(image, rad, Interpolation::Bilinear, LumaA([0, 0]))
+            ),
+            ImageRgb8(image) => ImageRgb8(
+                rotate_about_center(image, rad, Interpolation::Bilinear, Rgb([0, 0, 0]))
+            ),
+            ImageRgba8(image) => ImageRgba8(
+                rotate_about_center(image, rad, Interpolation::Bilinear, Rgba([0, 0, 0, 0]))
+            ),
+            ImageBgr8(image) => ImageBgr8(
+                rotate_about_center(image, rad, Interpolation::Bilinear, Bgr([0, 0, 0]))
+            ),
+            ImageBgra8(image) => ImageBgra8(
+                rotate_about_center(image, rad, Interpolation::Bilinear, Bgra([0, 0, 0, 0]))
+            ),
         }
     }
 }

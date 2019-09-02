b44b17f9dff6a619dc44e891ed80c7d46d48d4bb
Phil Ellison <phil.j.ellison@gmail.com>
2019-09-02 10:37:28 +0100

    Split op.rs into stack_ops.rs and image_ops.rs and move top-level pipline processing into main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,47 @@
 use std::path::PathBuf;
 use structopt::StructOpt;
-use image::{GenericImageView, open};
+use image::{DynamicImage, GenericImageView, open};
 
+mod stack;
+mod image_ops;
+use crate::image_ops::ImageOp;
+mod stack_ops;
+use crate::stack_ops::StackOp;
 mod error;
 use crate::error::Result;
 
-mod op;
-use op::run_pipeline;
+type ImageStack = stack::Stack<DynamicImage>;
 
-mod stack;
+// TODO:
+// - mechanism for taking an array of functions Image^n_i -> Image^m_i and producing a single
+//   function Image^{\Sigma n_i} -> Image^{\Sigma m_i}
+//  
+//  e.g. 2 x 2 grid of blurred images becomes:
+//      ... 'scale 0.4 > DUP 3 > [id, gaussian 5.0, gaussian 10.0, gaussian 15.0] > grid 2 2'
+//
+// - write grid properly, and either remove hcat and vcat or just make them shorthand for grid 2 1 and grid 1 2
+// - at the end of processing, read images off the stack and write as many as we have output names for
+//      example flows
+//          read two inputs
+//              first: blur
+//              second: sharpen
+//          finally: hcat blur sharpen
+// 
+//          read one input
+//              hcat (red channel input) (blue channel input)
+// 
+//          read one input 
+//              write red, blue and green channels as separate images
+//  - add constant images
+//  - add ability to add margins
+//  - support user-defined functions over pixels, e.g. map \x -> x * 0.3
+//  - add support for everything else relevant from imageproc
+//  - add validation for parsing of individual stages (make them subcommands somehow?)
+//  - add specific subcommands for the pipeline stages?
+//      e.g. make -p 'scale 0.4' equivalent to --scale 0.4
+//  - add stages with optional parameters. e.g. rotate uses image centre by default, but user
+//      can specify a centre of rotation if they like
+
 
 #[derive(StructOpt, Debug)]
 struct Opt {
@@ -47,4 +80,47 @@ fn main() -> Result<()> {
     output.save(&opt.output)?;
 
     Ok(())
+}
+
+// TODO: support returning multiple images
+fn run_pipeline(pipeline: &str, inputs: Vec<DynamicImage>, verbose: bool) -> DynamicImage {
+    // TODO: validation!
+    let ops: Vec<Op> = pipeline.split('>').map(|s| s.trim()).map(|s| Op::parse(s).unwrap()).collect();
+    let mut stack = ImageStack::new(inputs);
+
+    for op in ops {
+        if verbose {
+            println!("Applying {:?}", op);
+        }
+        op.apply(&mut stack);
+    }
+
+    stack.pop()
+}
+
+/// A pipeline operation - either a direct manipulation of the stack,
+/// or an image operation which reads from and writes to the top of the stack.
+#[derive(Debug)]
+pub enum Op {
+    StackOp(StackOp),
+    ImageOp(ImageOp),
+}
+
+impl Op {
+    fn parse(op: &str) -> Option<Op> {
+        match StackOp::parse(op) {
+            Some(o) => Some(Op::StackOp(o)),
+            None => match ImageOp::parse(op) {
+                Some(o) => Some(Op::ImageOp(o)),
+                None => None,
+            }
+        }
+    }
+
+    fn apply(&self, stack: &mut ImageStack) {
+        match self {
+            Op::StackOp(s) => s.apply(stack),
+            Op::ImageOp(o) => o.apply(stack),
+        }
+    }
 }

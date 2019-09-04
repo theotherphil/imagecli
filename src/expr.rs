66c76d0f0af93620512b7da1edad3229bfe4314a
Phil Ellison <phil.j.ellison@gmail.com>
2019-09-04 06:58:26 +0100

    Tidy expression parsing tests, fix bug

--- src/expr.rs
+++ src/expr.rs
@@ -32,12 +32,6 @@ impl Expr {
     }
 }
 
-#[test]
-fn test_expr() {
-    // 3 + 4 × 2 ÷ ( 1 − 5 ) ^ 2
-    // TODO!
-}
-
 /// Tokens in the DSL used to define user-provided functions.
 #[derive(Debug, Clone, PartialEq)]
 enum Token {
@@ -75,144 +69,6 @@ fn tokenise(func: &str) -> Vec<Token> {
     ]
 }
 
-#[test]
-fn test_parse_expr_num() {
-    let tokens = vec![Token::Num(2.0)];
-    let expected = Expr::Num(2.0);
-    let expr = parse_expr(&tokens);
-    assert_eq!(expr, expected);
-}
-
-#[test]
-fn test_parse_expr_var() {
-    let tokens = vec![Token::Var("x".into())];
-    let expected = Expr::Var("x".into());
-    let expr = parse_expr(&tokens);
-    assert_eq!(expr, expected);
-}
-
-#[test]
-fn test_parse_expr_add() {
-    // 3 + 4
-    let tokens = vec![
-        Token::Num(3.0),
-        Token::Plus,
-        Token::Num(4.0),
-    ];
-    let expected = Expr::Binary(
-        ArithmeticOp::Add,
-        Box::new(Expr::Num(3.0)),
-        Box::new(Expr::Num(4.0)),
-    );
-    let expr = parse_expr(&tokens);
-    assert_eq!(expr, expected);
-}
-
-#[test]
-fn test_parse_expr_add_mul() {
-    // 3 + 4 * 2
-    let tokens = vec![
-        Token::Num(3.0),
-        Token::Plus,
-        Token::Num(4.0),
-        Token::Mul,
-        Token::Num(2.0),
-    ];
-    let expected = Expr::Binary(
-        ArithmeticOp::Add,
-        Box::new(Expr::Num(3.0)),
-        Box::new(
-            Expr::Binary(
-                ArithmeticOp::Mul,
-                Box::new(Expr::Num(4.0)),
-                Box::new(Expr::Num(2.0))
-            )
-        )
-    );
-    let expr = parse_expr(&tokens);
-    assert_eq!(expr, expected);
-}
-
-#[test]
-fn test_parse_expr_add_mul_parens() {
-    // (3 + 4) * 2
-    let tokens = vec![
-        Token::OpenParen,
-        Token::Num(3.0),
-        Token::Plus,
-        Token::Num(4.0),
-        Token::CloseParen,
-        Token::Mul,
-        Token::Num(2.0),
-    ];
-    let expected = Expr::Binary(
-        ArithmeticOp::Mul,
-        Box::new(
-            Expr::Binary(
-                ArithmeticOp::Add,
-                Box::new(Expr::Num(3.0)),
-                Box::new(Expr::Num(4.0))
-            )
-        ),
-        Box::new(Expr::Num(2.0)),
-    );
-    let expr = parse_expr(&tokens);
-    assert_eq!(expr, expected);
-}
-
-#[test]
-fn test_parse_expr_complex() {
-    //3 + 4 × 2 ÷ ( 1 − 5 ) ^ 2
-    let tokens = vec![
-        Token::Num(3.0),
-        Token::Plus,
-        Token::Num(4.0),
-        Token::Mul,
-        Token::Num(2.0),
-        Token::Div,
-        Token::OpenParen,
-        Token::Num(1.0),
-        Token::Sub,
-        Token::Num(5.0),
-        Token::CloseParen,
-        Token::Pow,
-        Token::Num(2.0),
-    ];
-    
-    //3 + 4 × 2 ÷ ( 1 − 5 ) ^ 2
-    let expected = Expr::Binary(
-        ArithmeticOp::Add,
-        Box::new(Expr::Num(3.0)),
-        Box::new(
-            Expr::Binary(
-                ArithmeticOp::Div,
-                Box::new(
-                    Expr::Binary(
-                        ArithmeticOp::Mul,
-                        Box::new(Expr::Num(4.0)),
-                        Box::new(Expr::Num(2.0))
-                    )
-                ),
-                Box::new(
-                    Expr::Binary(
-                        ArithmeticOp::Exp,
-                        Box::new(
-                            Expr::Binary(
-                                ArithmeticOp::Sub,
-                                Box::new(Expr::Num(1.0)),
-                                Box::new(Expr::Num(5.0))
-                            )
-                        ),
-                        Box::new(Expr::Num(2.0))
-                    )
-                )
-            )
-        )
-    );
-    let expr = parse_expr(&tokens);
-    assert_eq!(expr, expected);
-}
-
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 enum Associativity { Right, Left }
 
@@ -221,7 +77,7 @@ fn precedence(token: &Token) -> u32 {
         Token::Plus => 2,
         Token::Sub => 2,
         Token::Mul => 3,
-        Token::Div => 4,
+        Token::Div => 3,
         Token::Pow => 4,
         // TODO: introduce a new enum for just the tokens?
         _ => panic!("Not an operator"),
@@ -285,41 +141,21 @@ fn parse_expr(tokens: &[Token]) -> Expr {
     let mut operator_stack = Vec::new();
 
     for token in tokens {
-    //     read a token.
         match token {
-    //     if the token is a number, then:
-    //         push it to the output queue.
             Token::Num(n) => output_queue.push(Expr::Num(*n)),
             Token::Var(v) => output_queue.push(Expr::Var(v.clone())),
-    //     if the token is a function then:
-    //         push it onto the operator stack 
-
             // TODO
             //Token::Func(_) => operator_stack.push(token.clone()),
             Token::Func(_) => panic!("Functions are not yet supported"),
-
-    //     if the token is an operator, then:
-    //         while (
-    //          (
-    //              (there is a function at the top of the operator stack)
-    //              or (there is an operator at the top of the operator stack with greater precedence)
-    //              or (the operator at the top of the operator stack has equal precedence and is left associative)
-    //          )
-    //          and (the operator at the top of the operator stack is not a left parenthesis
-    //         ):
-    //             pop operators from the operator stack onto the output queue.
-    //         push it onto the operator stack.
             Token::Plus | Token::Sub | Token::Mul | Token::Div | Token::Pow => {
                 loop {
                     if operator_stack.is_empty() {
                         break;
                     }
                     let top = &operator_stack[operator_stack.len() - 1];
-                    if (
-                            is_function(top)
-                            || (is_operator(top) && precedence(top) > precedence(token))
-                            || (is_operator(top) && precedence(top) == precedence(token) && associativity(top) == Associativity::Left)
-                        ) // && !is_lparen(top) <-- in wiki article, but redundant. Suspicious...
+                    if is_function(top)
+                        || (is_operator(top) && precedence(top) > precedence(token))
+                        || (is_operator(top) && precedence(top) == precedence(token) && associativity(top) == Associativity::Left)
                     {
                         let top = operator_stack.pop().unwrap();
                         add_node(&mut output_queue, arithmetic_op(&top));
@@ -329,26 +165,23 @@ fn parse_expr(tokens: &[Token]) -> Expr {
                 }
                 operator_stack.push(token.clone())
             },
-    //     if the token is a left paren (i.e. "("), then:
-    //         push it onto the operator stack.
             Token::OpenParen => operator_stack.push(token.clone()),
-    //     if the token is a right paren (i.e. ")"), then:
-    //         while the operator at the top of the operator stack is not a left paren:
-    //             pop the operator from the operator stack onto the output queue.
-    //         /* if the stack runs out without finding a left paren, then there are mismatched parentheses. */
-    //         if there is a left paren at the top of the operator stack, then:
-    //             pop the operator from the operator stack and discard it
             Token::CloseParen => {
+                let mut matched = false;
                 loop {
                     if operator_stack.is_empty() {
                         break;
                     }
                     let top = &operator_stack[operator_stack.len() - 1];
                     if is_lparen(top) {
+                        matched = true;
                         break;
                     }
                     let top = operator_stack.pop().unwrap();
                     add_node(&mut output_queue, arithmetic_op(&top));
+                }
+                if !matched {
+                    panic!("Unmatched parenthesis");
                 }
                 if !operator_stack.is_empty() {
                     let top = &operator_stack[operator_stack.len() - 1];
@@ -360,7 +193,6 @@ fn parse_expr(tokens: &[Token]) -> Expr {
         }
     }
 
-    // after while loop, if operator stack not null, pop everything to output queue
     loop {
         if operator_stack.is_empty() {
             break;
@@ -369,13 +201,114 @@ fn parse_expr(tokens: &[Token]) -> Expr {
         add_node(&mut output_queue, arithmetic_op(&top));
     }
 
-    // if there are no more tokens to read then:
-    //     while there are still operator tokens on the stack:
-    //         /* if the operator token on the top of the stack is a paren, then there are mismatched parentheses. */
-    //         pop the operator from the operator stack onto the output queue.
-    
-    // TODO: ^^ ??? I've already read all the tokens and popped everything from the stack... 
-    
-    println!("OUTPUT QUEUE: {:#?}", output_queue);
     output_queue.pop().unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Macros to make it less verbose to write test expectations involving Exprs
+    macro_rules! op {
+        ($op:expr, $left:expr, $right:expr) => { Expr::Binary($op, Box::new($left), Box::new($right)) }
+    }
+    macro_rules! add { ($left:expr, $right:expr) => {  op!(ArithmeticOp::Add, $left, $right) } }
+    macro_rules! sub { ($left:expr, $right:expr) => {  op!(ArithmeticOp::Sub, $left, $right) } }
+    macro_rules! mul { ($left:expr, $right:expr) => {  op!(ArithmeticOp::Mul, $left, $right) } }
+    macro_rules! div { ($left:expr, $right:expr) => {  op!(ArithmeticOp::Div, $left, $right) } }
+    macro_rules! exp { ($left:expr, $right:expr) => {  op!(ArithmeticOp::Exp, $left, $right) } }
+    macro_rules! num { ($num:expr) => { Expr::Num($num) } }
+
+    // Macro to make it less verbose to list Tokens in tests
+    macro_rules! tokens {
+        ($( $t:expr ),*) => {
+            vec![$( tok($t) ),* ]
+        }
+    }
+    // TODO: handle named functions
+    fn tok(t: &str) -> Token {
+        if let Ok(n) = t.parse::<f32>() {
+            return Token::Num(n);
+        }
+        match t {
+            "(" => Token::OpenParen,
+            ")" => Token::CloseParen,
+            "+" => Token::Plus,
+            "-" => Token::Sub,
+            "*" => Token::Mul,
+            "/" => Token::Div,
+            "^" => Token::Pow,
+            _ => Token::Var(t.into())
+        }
+    }
+
+    #[test]
+    fn test_parse_expr_num() {
+        let tokens = vec![Token::Num(2.0)];
+        let expected = Expr::Num(2.0);
+        let expr = parse_expr(&tokens);
+        assert_eq!(expr, expected);
+    }
+
+    #[test]
+    fn test_parse_expr_var() {
+        let tokens = vec![Token::Var("x".into())];
+        let expected = Expr::Var("x".into());
+        let expr = parse_expr(&tokens);
+        assert_eq!(expr, expected);
+    }
+
+    #[test]
+    fn test_parse_expr_add() {
+        // 3 + 4
+        let tokens = tokens!["3", "+", "4"];
+        let expected = add!(num!(3.0), num!(4.0));
+        let expr = parse_expr(&tokens);
+        assert_eq!(expr, expected);
+    }
+
+    #[test]
+    fn test_parse_expr_add_mul() {
+        // 3 + 4 * 2
+        let tokens = tokens!["3", "+", "4", "*", "2"];
+        let expected = add!(num!(3.0), mul!(num!(4.0), num!(2.0)));
+        let expr = parse_expr(&tokens);
+        assert_eq!(expr, expected);
+    }
+
+    #[test]
+    fn test_parse_expr_add_mul_parens() {
+        // (3 + 4) * 2
+        let tokens = tokens!["(", "3", "+", "4", ")", "*", "2"];
+        let expected = mul!(add!(num!(3.0), num!(4.0)), num!(2.0));
+        let expr = parse_expr(&tokens);
+        assert_eq!(expr, expected);
+    }
+
+    #[test]
+    fn test_parse_expr_mul_div() {
+        // 4 x 2 / 5
+        let tokens = tokens!["4", "*", "2", "/", "5"];
+        let expected = div!(mul!(num!(4.0), num!(2.0)), num!(5.0));
+        let expr = parse_expr(&tokens);
+        assert_eq!(expr, expected);
+    }
+
+    #[test]
+    fn test_parse_expr_complex() {
+        // 3 + 4 × 2 ÷ ( 1 − 5 ) ^ 2
+        let tokens = tokens!["3", "+", "4", "*", "2", "/", "(", "1", "-", "5", ")", "^", "2"];
+        let expected = add!(
+            num!(3.0),
+            div!(
+                mul!(num!(4.0), num!(2.0)),
+                exp!(
+                    sub!(num!(1.0), num!(5.0)),
+                    num!(2.0)
+                )
+            )
+        );
+        let expr = parse_expr(&tokens);
+        assert_eq!(expr, expected);
+    }
 }
